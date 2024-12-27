0879ea9f11a380bdf1e1853c3af509e014903736
XvSvM <vadiagadia@gmail.com>
2024-12-27 19:19:51 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -8,6 +8,23 @@
 # Soong namespaces
 LOCAL_PATH := device/askey/adt3
 
+# A/B
+AB_OTA_PARTITIONS += recovery
+AB_OTA_UPDATER := true
+ENABLE_VIRTUAL_AB := true
+TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 PRODUCT_PACKAGES += \
     linker.vendor_ramdisk \
     shell_and_utilities_vendor_ramdisk \

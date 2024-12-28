da4c8e41010dc40b939650fa156829923920d442
XvSvM <vadiagadia@gmail.com>
2024-12-28 22:29:39 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,97 +11,50 @@ DEVICE_PATH := device/askey/adt3
 PRODUCT_SHIPPING_API_LEVEL := 33
 PRODUCT_TARGET_VNDK_VERSION := 34
 
-# A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    product \
-    system \
-    system_ext \
-    vendor 
-  
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier \
-    otapreopt_script \
-    checkpoint_gc
 
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/askey/adt3
+# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE) \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=$(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE) \
-    POSTINSTALL_OPTIONAL_vendor=true
 
-# Additional Target Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.graphics.common@1.0 \
-    libion \
-    libxml2
-
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.graphics.common@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
-
-# Bootctrl
+# boot hal
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service \
     android.hardware.boot@1.2-impl \
     android.hardware.boot@1.2-impl.recovery
 
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
-
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4
+   bootctrl.updateboot \
+   update_engine_sideload \
+   update_boot   \
+   update_recovery_boot   \
+   update_engine  \
+   update_verifier
 
-# Health
+
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    bootctrl.diana \
+    libgptutils \
+    libz \
+    libcutils
 
-# HIDL Service
-PRODUCT_ENFORCE_VINTF_MANIFEST := true
-
-# Recovery init
 PRODUCT_PACKAGES += \
-    init.recovery.diana.rc
-
-# Filesystem table
-PRODUCT_PACKAGES += \
-    recovery.fstab \
-
-# fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
-# Gatekeeper
-PRODUCT_PACKAGES += \
-	android.hardware.gatekeeper@1.0-service
-
-# Additional Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libkeymaster41 \
-    libpuresoftkeymasterdevice
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-
-# Rootdir
-PRODUCT_PACKAGES += \
-    servicemanager.recovery.rc \
-    snapuserd.rc
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
